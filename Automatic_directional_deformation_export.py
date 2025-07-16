f6198177f595abd11a985f3d2f6ca547d4fe0714
gregorcjaka <gregorcjaka@gmail.com>
2025-07-16 20:55:59 +0200

    Update Automatic_directional_deformation_export.py

--- Automatic_directional_deformation_export.py
+++ Automatic_directional_deformation_export.py
@@ -5,14 +5,9 @@ Pred zagonom spremenimo izhodno datoteko in vektor time_points na casovne tocke,
 """
 
 import os
-
 export_folder = r"C:\Location\for\results"
 time_points = [0.2, 0.4, 0.6, 0.8, 1.0]
-directions = [
-    ("X", NormalOrientationType.XAxis),
-    ("Y", NormalOrientationType.YAxis),
-    ("Z", NormalOrientationType.ZAxis)
-]
+directions = [("X", NormalOrientationType.XAxis), ("Y", NormalOrientationType.YAxis), ("Z", NormalOrientationType.ZAxis)]
 
 # 0) Preverimo, da mapa obstaja
 if not os.path.exists(export_folder):
@@ -30,7 +25,6 @@ for dir_label, dir_enum in directions:
         result.By = SetDriverStyle.Time
         result.DisplayTime = Quantity(str(t) + "[s]")
         result.CalculateTimeHistory = False
-
         result.EvaluateAllResults()
 
         # Shranimo pod ime datoteke: DirDeform_X_T1.txt, ...
